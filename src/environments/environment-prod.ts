7d0d031061776463bddaf90d26c4d21fdb7e7450
Marek Rezny <rezny.marek@gmail.com>
2025-05-06 16:42:37 +0200

    prod deployment3

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  keyCloakUrl: 'http://20.166.52.164',
+  keyCloakUrl: 'http://20.166.52.164/auth',
   beUrl: 'http://20.166.52.164',
   feUrl: 'http://20.166.52.164',
 };
